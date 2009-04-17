ca5e24b81ff24b852174b5f28ac8b8277936730e
Philipp A. Baer <philipp.baer@dfki.de>
2009-04-17 19:47:24 +0200

    Update for control structure (type and name changes). Control structure header is now copied over to the include dir.

--- ControlDevTypes.hpp
+++ ControlDevTypes.hpp
@@ -1,13 +1,14 @@
- //! Data structures for control devices and controllers
+#ifndef CONTROLDEVTYPES_HPP
+#define CONTROLDEVTYPES_HPP
 
 namespace controldev
 {
 
     enum DeviceActiveIdentifier
     {
-        None = 0,
-        Joystick = 1,
-        SliderBox = 2,
+        DAI_None = 0,
+        DAI_Joystick = 1,
+        DAI_SliderBox = 2
     };
 
     /** A data structure for raw data values of input devices.
@@ -16,16 +17,17 @@ namespace controldev
      */
     struct RawCommand
     {
-        DeviceActiveIdentifier devices;
+        int devices;
 
-        short joyLeftRight;           // The Linux API returns 16bit values
-        short joyUpDown;              // The Linux API returns 16bit values
-        short joySlide;               // not applicable for ASGuard
-        unsigned short joyThrottle;   // Throttle values are positive
-        unsigned short joyButtons;    // 16 buttons should be enough
+        short joyLeftRight;             // The Linux API returns 16bit values
+        short joyUpDown;                // The Linux API returns 16bit values
+        short joySlide;                 // not applicable for ASGuard
+        unsigned short joyThrottle;     // Throttle values are positive
+        unsigned short joyButtonCount;  // Number of buttons
+        unsigned short joyButtons;      // 16 buttons should be enough
 
-        short sliderValues[7];        // The slider box has seven slider values
-        unsigned char slilderButtons; // The slider box has four buttons
+        unsigned short sliderValues[7]; // The slider box has seven slider values
+        unsigned char sliderButtons;    // The slider box has four buttons
     };
 
     /** A unified motion control data structure for differential drive-based
@@ -41,3 +43,4 @@ namespace controldev
     };
 }
 
+#endif /* CONTROLDEVTYPES_HPP */
