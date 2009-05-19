deff05758d4ce4ce29a5b6d8881807626f390031
Sylvain Joyeux <sylvain.joyeux@dfki.de>
2009-05-19 13:34:53 +0000

    properly document the fields in MotionCommand

--- ControlDevTypes.hpp
+++ ControlDevTypes.hpp
@@ -35,8 +35,8 @@ namespace controldev
      */
     struct MotionCommand
     {
-        double translation;
-        double rotation;
+        double translation; //! translation value in m/s
+        double rotation;    //! rotation value in rad/s. Positive is counter-clockwise
         // That's actually enough information for a differential drive,
         // for an omnidirectional drive we would add a heading/angle
         // value.
