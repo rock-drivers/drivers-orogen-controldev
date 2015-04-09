6d5e149fff79b9cf0432f102183bb83d78f1b939
Sascha Arnold <sascha.arnold@dfki.de>
2015-04-09 13:55:24 +0200

    Revert changes to the joystick script

--- scripts/joystick.rb
+++ scripts/joystick.rb
@@ -2,9 +2,8 @@
 require 'orocos'
 include Orocos
 
-Orocos::CORBA.name_service.ip = "192.168.128.31"
-
 Orocos.initialize
+
 puts("starting")
 # spawn deployment
 Orocos.run 'controldev::JoystickTask' => 'Joystick' do
@@ -14,8 +13,25 @@ Orocos.run 'controldev::JoystickTask' => 'Joystick' do
     joy.configure
     joy.start
 
+    raw_reader = joy.raw_command.reader
+    m2d_reader = joy.motion_command.reader
     
     while(1)
+        if raw = raw_reader.read_new
+            if(raw.axisValue.size == 2)
+                if(raw.axisValue[0].size == 3 and raw.axisValue[1].size == 1)
+                    puts "#{raw.axisValue[0][0]} #{raw.axisValue[0][1]} #{raw.axisValue[0][2]} #{raw.axisValue[1][0]}"
+                else
+                    STDOUT.puts "Warning sizes ar invalud: #{raw.axisValue[0].size} #{raw.axisValue[1].size}"
+                end
+            else
+                STDOUT.puts "Warning sizes ar invalud: #{raw.AxisValue.size}"
+            end
+        end
+        if m2d = m2d_reader.read_new
+            #puts "#{m2d.translation} #{m2d.rotation}"
+        end
+        sleep 0.1
     end
 
 end
